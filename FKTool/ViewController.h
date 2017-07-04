1db48305bc3a1d4b75245389233a52114e519a6c
ForKid <910784384@qq.com>
2017-07-04 14:47:57 +0800

    init

---
+++ FKTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FKTool
+//
+//  Created by 周宏辉 on 2017/7/4.
+//  Copyright © 2017年 ForKid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
